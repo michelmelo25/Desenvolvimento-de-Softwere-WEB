96a50e5b0155c3b828a1e688a26a25af0913729b
Michel Melo <mmelo0425@gmail.com>
2018-04-16 16:50:20 -0300

    add dos botoes mais e menos noticia

---
+++ Pags/Exercicio JQuery/News/js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+    // Activate Carousel
+    $("#myCarousel").carousel();
+    
+    // Enable Carousel Indicators
+    $(".item0").click(function(){
+        $("#carouselExampleIndicators").carousel(0);
+    });
+    $(".item1").click(function(){
+        $("#carouselExampleIndicators").carousel(1);
+    });
+    $(".item2").click(function(){
+        $("#carouselExampleIndicators").carousel(2);
+    });
+    
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#carouselExampleIndicators").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#carouselExampleIndicators").carousel("next");
+    });
+});
+
